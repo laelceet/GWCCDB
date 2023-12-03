cb1ec97c7d1f0e5f807f497d11c77221fc944370
Lauren <114707994+laelceet@users.noreply.github.com>
2023-12-03 12:59:07 -0500

    Update indexes.sql
    created two more indexes for finding the student and facilitator names

--- indexes.sql
+++ indexes.sql
@@ -1,3 +1,11 @@
 /* create an index for the course table using the attributes semester and year */
 create index course_index
 on course(semester, year);
+
+/* create an index for the student table using the attributes first_name and last_name */
+create index student_name_index
+on student(first_name, last_name);
+
+/* create an index for the facilitator table using the attributes first_name and last_name */
+create index facilitator_name_index
+on facilitator(first_name, last_name);
