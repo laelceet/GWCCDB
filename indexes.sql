e8685f20184fa253005d10464bec81dc67167309
Lauren <114707994+laelceet@users.noreply.github.com>
2023-12-03 19:01:00 -0500

    Update indexes.sql
    deleted the facilitator index since it is not used

--- indexes.sql
+++ indexes.sql
@@ -5,7 +5,3 @@ on course(semester, year);
 /* create an index for the student table using the attributes first_name and last_name */
 create index student_name_index
 on student(first_name, last_name);
-
-/* create an index for the facilitator table using the attributes first_name and last_name */
-create index facilitator_name_index
-on facilitator(first_name, last_name);
